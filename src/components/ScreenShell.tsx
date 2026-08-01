51a35bbb08a73d38aded6382f5e9fc3216370db8
gordiontenor <118683044+gordiontenor@users.noreply.github.com>
2026-08-01 23:49:39 +0300

    chore: eslint setup, lint fixes, error msg tests, settings pills

--- src/components/ScreenShell.tsx
+++ src/components/ScreenShell.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, StyleSheet, StatusBar } from "react-native";
+import { StyleSheet, StatusBar } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { darkTheme } from "../theme/theme";
 

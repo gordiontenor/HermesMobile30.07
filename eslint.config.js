7038c21797864ce419456073f58ffd0abf6345ea
gordiontenor <118683044+gordiontenor@users.noreply.github.com>
2026-08-01 23:49:43 +0300

    chore: eslint setup, lint fixes, error msg tests, settings pills

---
+++ eslint.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'eslint/config';
+import expoConfig from 'eslint-config-expo/flat.js';
+
+export default defineConfig([
+  expoConfig,
+  {
+    rules: {
+      "react-hooks/refs": "off",
+    },
+  },
+]);
